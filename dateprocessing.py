f76e71356050571402b5deae72a283306354b169
Amir More <habeanf@gmail.com>
2012-08-25 16:45:13 +0300

    Integrated date processing

--- dateprocessing.py
+++ dateprocessing.py
@@ -1,39 +1,13 @@
 #coding: utf-8
-from lxml import etree
 import re
-from itertools import compress
-import pickle
 import datetime
-#content = open('original.html').read().decode('utf8').encode('ascii', 'replace')
-#@print (content)
-tree = etree.parse("original.html",etree.HTMLParser())
-
-states = tree.xpath("/html/body/div[@id='content']/div[@id='bodyContent']/div[@id='mw-content-text']/h3")
-other = tree.xpath("/html/body/div[@id='content']/div[@id='bodyContent']/div[@id='mw-content-text']/p")[1:]
-tables = tree.xpath("/html/body/div[@id='content']/div[@id='bodyContent']/div[@id='mw-content-text']/table[@class='wikitable']")
-
-pairs = zip(states,other,tables)
-
-pctre = re.compile('(\d+|\d+\.\d+)\%.(\d+|\d+\.\d+)\%')
-
-samplere = re.compile('.*Sample size:.*(Varies|\d+).*')
-
-def getpcts(raw):
-	d = pctre.match(raw)
-	return float(d.group(1)),float(d.group(2))
 
 class DateFromTo(object):
 	def __init__(self, startDate, endDate) :
 		self.startDate = startDate
 		self.endDate = endDate
-
-dates = []
-for state,other,tab in pairs[:46]:
-	for row in tab.xpath("tr")[1:]:
-		if len(row.xpath("td/a|td/br"))>2:
-			dates.append(row.xpath("td[2]/text()"))
-		if len(row.xpath("td/p/a|td/p/br"))>2:
-			dates.append(row.xpath("td[2]/text()"))
+	def __str__(self):
+		return '('+str(self.startDate)+','+str(self.endDate)+')'
 
 regexStr = """
 ^
@@ -54,7 +28,7 @@ monthsText = ["January","February","March","April","May","June","July","August",
 def cleanStr(astr):
 	return astr.replace(' ','').encode('utf8').replace('–','-').encode('ascii')
 
-def blah(x):
+def getDateSpan(x):
 	s = cleanStr(x[0])
 	try:
 		regxRes = regex.search( s )
@@ -67,10 +41,3 @@ def blah(x):
 	except Exception, err:
 		print err
 		print( "string:" + s )
-
-structuredDates = [ blah(ds)  for ds in dates ]
-#print( dates )
-for sd in structuredDates:
-	print(sd.startDate, sd.endDate)
-
-# map(lambda x: print("s"),  structuredDates )
--- stateprocessing.py
+++ stateprocessing.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python
+
 from lxml import etree
 import re
 from itertools import compress
+from pprint import pprint
+from dateprocessing import getDateSpan,DateFromTo
 
-tree = etree.parse("original.html",etree.HTMLParser())
+tree = etree.parse("original3.html",etree.HTMLParser())
 
 states = tree.xpath("/html/body/div[@id='content']/div[@id='bodyContent']/div[@id='mw-content-text']/h3")
 other = tree.xpath("/html/body/div[@id='content']/div[@id='bodyContent']/div[@id='mw-content-text']/p")[1:]
-tables = tree.xpath("/html/body/div[@id='content']/div[@id='bodyContent']/div[@id='mw-content-text']/table[@class='wikitable']")
+tables = tree.xpath("/html/body/div[@id='content']/div[@id='bodyContent']/div[@id='mw-content-text']/table[@class='wikitable']|/html/body/div[@id='content']/div[@id='bodyContent']/div[@id='mw-content-text']/div[@class='rellink boilerplate seealso']")
 
 pairs = zip(states,other,tables)
 
@@ -22,6 +26,27 @@ def getpcts(raw):
 
 stateresults=[]
 
+def getpollresults(row,isfirst):
+	democrat = None
+	demopct = None
+	republic = None
+	repubpct = None
+	leadmargin = None
+	if isfirst:
+		democrat = row.xpath("td[3]/text()|td[3]/a/text()|td[3]/a/b/text()|td[3]/b/a/text()|td[3]/b/text()")[0]
+		demopct = row.xpath("td[4]/text()|td[4]/a/text()|td[4]/a/b/text()|td[4]/b/a/text()|td[4]/b/text()")[0]
+		republican = row.xpath("td[5]/text()|td[5]/a/text()|td[5]/a/b/text()|td[5]/b/a/text()|td[5]/b/text()")[0]
+		repubpct = row.xpath("td[6]/text()|td[6]/a/text()|td[6]/a/b/text()|td[6]/b/a/text()|td[6]/b/text()")[0]
+		leadmargin = row.xpath("td[7]/text()|td[7]/a/text()|td[7]/a/b/text()|td[7]/b/a/text()|td[7]/b/text()")[0]
+	else:
+		democrat = row.xpath("td[1]/text()|td[1]/a/text()|td[1]/a/b/text()|td[1]/b/a/text()|td[1]/b/text()")[0]
+		demopct = row.xpath("td[2]/text()|td[2]/a/text()|td[2]/a/b/text()|td[2]/b/a/text()|td[2]/b/text()")[0]
+		republican = row.xpath("td[3]/text()|td[3]/a/text()|td[3]/a/b/text()|td[3]/b/a/text()|td[3]/b/text()")[0]
+		repubpct = row.xpath("td[4]/text()|td[4]/a/text()|td[4]/a/b/text()|td[4]/b/a/text()|td[4]/b/text()")[0]
+		leadmargin = row.xpath("td[5]/text()|td[5]/a/text()|td[5]/a/b/text()|td[5]/b/a/text()|td[5]/b/text()")[0]
+	return (democrat,float(demopct[:-1]),republican,float(repubpct[:-1]),float(leadmargin) if leadmargin not in ['Tie','Tied'] else 0.0)
+
+
 for state,other,tab in pairs[:46]:
 	newstate = dict()
 	newstate['name']=state.xpath("span[@class='mw-headline']/a")[0].text
@@ -29,14 +54,14 @@ for state,other,tab in pairs[:46]:
 	print state.xpath("span[@class='mw-headline']/a")[0].text
 	data = map(lambda x:getpcts(x[2:]),compress(other.xpath("text()"),[0,1,0,1]))
 	newstate['2004']=(other.xpath("a[1]/text()")[0].split(' ')[0],data[0])
-	newstate['2004']=(other.xpath("a[2]/text()")[0].split(' ')[0],data[1])
+	newstate['2008']=(other.xpath("a[2]/text()")[0].split(' ')[0],data[1])
 	polls = []
 	for row in tab.xpath("tr")[1:]:
 		if len(row.xpath("td/a|td/br"))>2:
 			newpoll = dict()
 			newpoll['pollers'] = map(lambda x:x.strip(),row.xpath("td[1]/a/text()"))
 			newpoll['url'] = row.xpath("td[1]/a/@href")
-			newpoll['date struct'] = row.xpath("td[2]/text()")
+			newpoll['date struct'] = getDateSpan(row.xpath("td[2]/text()"))
  			print map(lambda x:x.strip(),row.xpath("td[1]/a/text()")),
 			print row.xpath("td[2]/text()")
 			samplesize = filter(lambda x:samplere.match(x.strip()),list(row.xpath("td[1]")[0].itertext()))[0]
@@ -49,11 +74,14 @@ for state,other,tab in pairs[:46]:
 			else:
 				newpoll['margin']=float(0)
 			polls.append(newpoll)
-		if len(row.xpath("td/p/a|td/p/br"))>2:
+			pollresults = getpollresults(row,True)
+			if pollresults[2] == 'Mitt Romney':
+				newpoll['results'] = pollresults
+		elif len(row.xpath("td/p/a|td/p/br"))>2:
 			newpoll = dict()
 			newpoll['pollers'] = map(lambda x:x.strip(),row.xpath("td[1]/p/a/text()"))
 			newpoll['url'] = row.xpath("td[1]/p/a/@href")
-			newpoll['date struct'] = row.xpath("td[2]/text()")
+			newpoll['date struct'] = getDateSpan(row.xpath("td[2]/text()"))
 			print map(lambda x:x.strip(),row.xpath("td[1]/p/a/text()")),
 			print row.xpath("td[2]/text()")
 			samplesize = filter(lambda x:samplere.match(x.strip()),list(row.xpath("td[1]/p")[0].itertext()))[0]
@@ -66,8 +94,17 @@ for state,other,tab in pairs[:46]:
 			newpoll['size'] = sampleresult.group('size')
 			newpoll['votertype'] = sampleresult.group('votertype')
 			polls.append(newpoll)
-		
+			pollresults = getpollresults(row,True)
+			if pollresults[2] == 'Mitt Romney':
+				newpoll['results'] = pollresults			
+		else:
+			pollresults = getpollresults(row,False)
+			if pollresults[2] == 'Mitt Romney':
+				newpoll['results'] = pollresults
+
+
 	newstate['polls']=polls
 	stateresults.append(newstate)
 
-#print stateresults
+
+pprint(stateresults)
